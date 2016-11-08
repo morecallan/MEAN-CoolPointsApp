51a1d00f52d211d8380cb23fbe0a01c8e0fb7041
Callan Morrison <callan.l.morrison@gmail.com>
2016-11-08 12:16:17 -0600

    connect routes to server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,19 @@
 const PORT = process.env.PORT || 3000
 const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 
+
 //////// MODULE IMPORTS: Express, body-parser ////////
 const {json} = require('body-parser');
 const express = require('express');
 const app = express();
+
+
+//////// ROUTES IMPORTS: user, instructor, student, assignment ////////
+const userRoutes = require('./routes/user');
+const instructorRoutes = require('./routes/instructor');
+const studentRoutes = require('./routes/student');
+const assignmentRoutes = require('./routes/assignment');
+
 
 //////// REDIS: Storing user session ////////
 const session = require('express-session')
@@ -18,7 +27,7 @@ app.use(session({
   'secret': 'cookiemonster'
 }))
 
-
+// Redis session sets app.locals each req
 app.use((req, res, next) => {
    //app.locals.email = req.session.email
    //app.locals.uid = req.session.uid
@@ -26,8 +35,9 @@ app.use((req, res, next) => {
 })
 
 //////// Middlewares ////////
-app.use(express.static('client'))
-app.use(json())
+app.use(express.static('client'));
+app.use(json());
+app.use(userRoutes, instructorRoutes, studentRoutes, assignmentRoutes);
 
 app.get('/', (req, res) => {
   console.log("hello world")
