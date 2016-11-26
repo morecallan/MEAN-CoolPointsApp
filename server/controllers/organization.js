5cc5c97cd4b563bb60a2a8d14bbc9051510f5291
Callan Morrison <callan.l.morrison@gmail.com>
2016-11-26 16:47:50 -0500

    updated organization bug and organizations now posting

--- server/controllers/organization.js
+++ server/controllers/organization.js
@@ -1,20 +1,19 @@
 'use strict';
 
-const User = require('../db/models/user');
 const Organization = require('../db/models/organization');
 
-const bcrypt = require('bcrypt');
-
 const createNewOrganization = ({body: {name}}, res) => {
-  //Check to see if user exists in the system with this email
+  //Check to see if organization exists in the system with this name
   Organization.find({name: name})
-  .then(()=>res.send({response: "This organization already exists."}))
-  .catch(()=>{
-  //Hashes users pawssword for storage.
-      return Organization.create({name: organizationName})
-    }).then((organization) => {
-        res.send({response: "User successfully created.", organization})
+  .then((results)=> {
+    if (results.length != 0) {
+      res.send({response: "This organization already exists."})
+    } else {
+      Organization.create({name: name}).then((organization) => {
+      res.send({response: "Organization successfully created.", organization})
     })
+  }
+  })
 }
 
 module.exports = {createNewOrganization}
